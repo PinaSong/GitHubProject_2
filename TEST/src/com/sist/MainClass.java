37a30a54d4107906af9f00237b9733047a617b79
user <user@DESKTOP-COJ9858>
2022-06-17 16:22:13 +0900

    123123

---
+++ TEST/src/com/sist/MainClass.java
@@ -0,0 +1,11 @@
+package com.sist;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello github");
+		// 형상관리
+	}	
+
+}
