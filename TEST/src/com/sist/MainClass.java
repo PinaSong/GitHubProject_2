3ea3e57a9b81697d37e4c16b466cc59b3b363b75
user <user@DESKTOP-COJ9858>
2022-06-17 16:35:26 +0900

    33124123

--- TEST/src/com/sist/MainClass.java
+++ TEST/src/com/sist/MainClass.java
@@ -6,6 +6,8 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		System.out.println("hello github");
 		System.out.println("hello github");
+		System.out.println("hello github");
+		System.out.println("hello github");
 		// 형상관리
 	}	
 
